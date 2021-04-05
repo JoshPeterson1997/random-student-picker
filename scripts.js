099ec3237266855e2f216b1c9d6be0763537f2bd
Joshua Peterson <petersonjoshua613@yahoo.com>
2021-04-05 13:07:23 -0500

    Added mt-medium and text-center

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-// TODO: Activate the 'random student' button to pick a random student
+// TODO: Activate the 'random student' button to pick a random train
+// TODO: `log` a random train's name when the button is clicked
+
+const form = document.querySelector("form");
+const trains = [];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+form.AddEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+const newTrain = Object.fromEntries(new FormData(event.target));
+
+trains.push(newTrain);
+
+document.querySelector(".button--jumbo").addEventListener("click", () => {
+  console.log("hello");
+});
